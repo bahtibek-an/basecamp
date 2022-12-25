bdfcc510ad9c5331a4ff319e638e22054d73f6e7
bahtibek-an <bahtibekanvarov@gmail.com>
2022-12-25 21:39:12 +0500

    add hot name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "basecamp-production-02e2.up.railway.app"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << "basecamp-production-02e2.up.railway.app"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
